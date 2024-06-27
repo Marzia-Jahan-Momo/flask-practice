1d99ca0f8a5b30c8686465a9502909c72e0020c9
jahanmomo <marziajahanmomo1971@gmail.com>
2024-06-28 01:19:28 +0600

    This is my 16th commit updated

--- controller_signup/user_controller.py
+++ controller_signup/user_controller.py
@@ -1,5 +1,6 @@
 from model_signup.user_model import user_model 
 from flask import request
+from datetime import datetime
 obj = user_model()
  
 def register_route(app):
@@ -27,4 +28,14 @@ def register_route(app):
     @app.route("/user/getall/limit/<limit>/page/<page>", methods=["GET"])
     def user_pagination_controller(limit, page):
         return obj.user_pagination_model(limit, page) 
-        
+    
+    @app.route("/user/<uid>/upload/avatar", methods=["PUT"])
+    def user_avatar_controller(uid):
+        file_obj = request.files["Avatar"]
+        uniqueFileName = str(datetime.now().timestamp()).replace(".", "") ## this is epoch time format
+        #return "This is user_upload_avatar_controller"
+        fileNameSplit = file_obj.filename.split(".")
+        extens = fileNameSplit[len(fileNameSplit)-1]
+        finalPath = f"uploads/{uniqueFileName}.{extens}"
+        file_obj.save(finalPath)
+        return obj.user_avatar_model(uid, finalPath)
